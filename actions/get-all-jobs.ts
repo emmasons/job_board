3aef4b2b1acc63797c3540aa095f01c75a1bda3f
xcixor <pndungu54@gmail.com>
2024-03-26 19:36:49 +0300

    Add country filter to getAllJobs action
    Extend the getAllJobs function to support filtering by a list of countries. The function now accepts an optional 'country' parameter, which can be a comma-separated list of countries. This list is then used to construct a condition for the database query, allowing for more granular job search results based on the specified countries. The existing location-based filtering logic has been adjusted to accommodate this new feature.

--- actions/get-all-jobs.ts
+++ actions/get-all-jobs.ts
@@ -4,16 +4,32 @@ import { JobsWithCompany } from "@/types/db";
 type Params = {
   title?: string;
   location?: string;
+  country?: string;
 };
 export const getAllJobs = async ({
   title,
   location,
+  country,
 }: Params): Promise<JobsWithCompany[]> => {
   const formattedTitle = title?.replace(/\s/g, "");
+  const countries = [];
+  if (country) {
+    const formattedCountries = country?.split(",");
+    countries.push(...formattedCountries);
+  }
   try {
+    // Define the list of countries
+
+    // Determine the country condition for the query
+    const countryCondition = location
+      ? { country: location } // If location is provided, use it for the query
+      : countries.length > 0
+      ? { country: { in: countries } } // If the country list exists and is not empty, use it
+      : {}; // Otherwise, do not apply any country filter
+
     const jobs = await db.job.findMany({
       where: {
-        country: location,
+        ...countryCondition, // Use the determined country condition
         ...(formattedTitle
           ? {
               OR: [
@@ -32,6 +48,7 @@ export const getAllJobs = async ({
         createdAt: "desc",
       },
     });
+
     return jobs;
   } catch (error) {
     console.log("GET_ALL_JOBS", error);
