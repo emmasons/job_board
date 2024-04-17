ec2dc213d6cc960dc233cced21758b1c0114ae83
xcixor <pndungu54@gmail.com>
2024-04-17 10:17:21 +0300

    Add new environment variables to create_env_file.sh
    Include new environment variables related to Google Cloud Storage and SMTP email configuration in the create_env_file.sh script. This update ensures all necessary variables are set in the .env file.

--- scripts/create_env_file.sh
+++ scripts/create_env_file.sh
@@ -13,6 +13,7 @@ create_env_file() {
     echo "GS_CREDENTIALS=${GS_CREDENTIALS}" >> .env
     echo "GS_BUCKET_NAME=${GS_BUCKET_NAME}" >> .env
     echo "GS_LOCATION=${GS_LOCATION}" >> .env
+    echo "GS_PUBLIC_BUCKET_NAME=${GS_PUBLIC_BUCKET_NAME}" >> .env
     echo "SERVICE_ID=${SERVICE_ID}" >> .env
     echo "DEFAULT_TEMPLATE_ID=${DEFAULT_TEMPLATE_ID}" >> .env
     echo "PUBLIC_KEY=${PUBLIC_KEY}" >> .env
@@ -20,6 +21,13 @@ create_env_file() {
     echo "ADMIN_EMAIL=${ADMIN_EMAIL}" >> .env
     echo "BASE_DOMAIN=${BASE_DOMAIN}" >> .env
     echo "NODE_ENV=${NODE_ENV}" >> .env
+
+    # email 
+    echo "SMTP_EMAIL_HOST=${SMTP_EMAIL_HOST}" >> .env
+    echo "SMTP_EMAIL_PORT=${SMTP_EMAIL_PORT}" >> .env
+    echo "SMTP_AUTH_USER=${SMTP_AUTH_USER}" >> .env
+    echo "SMTP_AUTH_PASSWORD=${SMTP_AUTH_PASSWORD}" >> .env
+    echo "TEST_RECIPIENT=${TEST_RECIPIENT}" >> .env
 }
 main(){
     create_env_file
