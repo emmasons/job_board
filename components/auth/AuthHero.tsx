88a031a98c8670768eb79b21c5f00d5c70845b11
javaGhost1 <dennismurage97@gmail.com>
2024-06-25 11:16:44 +0300

    Update AuthHero component with new portal name and revised text.

--- components/auth/AuthHero.tsx
+++ components/auth/AuthHero.tsx
@@ -5,10 +5,10 @@ const AuthHero = () => {
     <div className="flex h-full flex-col justify-end bg-[url('/auth/ladylogin.jpg')] bg-cover bg-center bg-no-repeat px-4 pb-24">
       <div className="rounded-lg bg-[rgba(64,64,64,0.7)] px-4 py-8 text-secondary">
         <h2 className="text-xl font-bold">
-          Welcome to Infinite Talent Europe Job Portal
+          Welcome to Jobs Connect Gulf Job Portal
         </h2>
         <p className="text-[0.9rem]">
-          Join thousands of other talentseekers and employers.
+          Join thousands of other job seekers and employers.
         </p>
       </div>
     </div>
