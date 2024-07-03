385c2ace8229177131368a68393c533f1c1d7020
xcixor <pndungu54@gmail.com>
2024-07-03 19:24:25 +0300

    Add logo and tagline to loading screen

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,10 +1,13 @@
+import { Logo } from "@/components/navbar/Logo";
 import { Loader2 } from "lucide-react";
 import React from "react";
 
 const loading = () => {
   return (
-    <div className="flex h-screen w-full items-center justify-center bg-slate-100">
-      <Loader2 className="h-20 w-20 animate-spin text-primary" />
+    <div className="flex gap-4 h-screen w-full items-center justify-center bg-slate-100 flex-col">
+      <Logo />
+      <p className="text-secondary">Where talent and jobs meet.</p>
+      <Loader2 className="h-16 w-16 animate-spin text-primary" />
     </div>
   );
 };
