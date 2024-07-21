1e1b25f022c4aef29bed174e22ddfa84d878bc86
xcixor <pndungu54@gmail.com>
2024-07-22 00:35:26 +0300

    feat(richeditor): update toolbar

--- components/ckeditor/RichTextRenderer.tsx
+++ components/ckeditor/RichTextRenderer.tsx
@@ -1,44 +1,13 @@
 "use client";
-
-import dynamic from "next/dynamic";
-import React from "react";
-import { useMemo } from "react";
-
-import "react-quill/dist/quill.bubble.css";
+import "ckeditor5/ckeditor5.css";
+import "./ck_style.css";
 
 interface PreviewProps {
   value: string;
 }
 
 export const Preview = ({ value }: PreviewProps) => {
-  const ReactQuill = useMemo(
-    () => dynamic(() => import("react-quill"), { ssr: false }),
-    [],
-  );
-
-  // Override the default style of the react-quill package
-  // so that the font is size 1.4rem
-  React.useEffect(() => {
-    const style = document.createElement("style");
-    style.innerHTML = `
-      .ql-editor {
-        font-size: 1rem;
-        padding: 0;
-        font-family: sans-serif;
-      }
-    `;
-    document.head.appendChild(style);
-
-    return () => {
-      document.head.removeChild(style);
-    };
-  }, []);
-
   return (
-    <ReactQuill
-      theme="bubble"
-      value={value}
-      readOnly
-    />
+    <div className="ck-content" dangerouslySetInnerHTML={{ __html: value }} />
   );
 };
