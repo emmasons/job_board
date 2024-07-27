b0dba4fdcaeb59ee815cf330b21a34734dad393b
xcixor <pndungu54@gmail.com>
2024-07-27 16:41:36 +0300

    Add Employer Profile Retrieval and Enhance Employer Dashboard
    - Implemented a new function `getEmployerProfile` in `actions/employer/get-profile.ts` to fetch detailed employer profiles, including company, address, and associated jobs, enhancing the ability to display comprehensive employer information.
    - Updated the job creation API endpoint in `app/api/jobs/route.ts` to correctly associate jobs with the employer's company, ensuring data integrity and proper relationship mapping.
    - Enhanced the email verification page UI in `app/auth/verify-email/page.tsx` by adjusting the background color for better readability and visual consistency.
    - Introduced a new page component `app/profile/dashboard/employer/company/[id]/page.tsx` for displaying specific company details, although it currently returns a placeholder.
    - Significantly improved the employer dashboard UI in `app/profile/dashboard/employer/page.tsx` to display detailed company information, job statistics, and candidate counts, providing a more informative and user-friendly interface for employers.
    - Added a new migration script in `prisma/migrations/20240727120146_042/migration.sql` to introduce a `JOBSTATUS` enum and associate it with the `Job` model, enabling better job status management within the application.
    - Updated the Prisma schema in `prisma/schema.prisma` to reflect the new `JOBSTATUS` field and made minor adjustments to model definitions for consistency and accuracy.

--- app/profile/dashboard/employer/page.tsx
+++ app/profile/dashboard/employer/page.tsx
@@ -1,7 +1,17 @@
+import { getEmployerProfile } from "@/actions/employer/get-profile";
 import { getCompanyForUser } from "@/actions/get-company-for-user";
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import { getCurrentSessionUser } from "@/lib/auth";
-import { Role } from "@prisma/client";
-import { BriefcaseIcon, ChevronRight } from "lucide-react";
+import { JOBSTATUS, Role } from "@prisma/client";
+import {
+  BriefcaseIcon,
+  ChevronRight,
+  Hammer,
+  Mail,
+  MapPin,
+  Pencil,
+  PhoneIcon,
+} from "lucide-react";
 import Link from "next/link";
 import { redirect } from "next/navigation";
 
@@ -10,26 +20,93 @@ const page = async () => {
   if (!user || !(user.role === Role.EMPLOYER)) {
     return redirect("/");
   }
-  const company = await getCompanyForUser(user.id);
+  // const company = await getCompanyForUser(user.id);
+  const employer = await getEmployerProfile(user.id);
+  if (!employer) {
+    return <div>Profile not found</div>;
+  }
+  console.log("employerProfile?", employer.employerProfile);
   return (
-    <div className="p-6">
-      <h1 className="my-4 flex items-center gap-4 text-2xl font-bold">
-        Company Settings
-        <BriefcaseIcon className="h-6 w-6 text-primary" />
-      </h1>
-      {company ? (
-        <Link href={`/profile/dashboard/staff/company/${company.id}`}>
-          Company: {company.companyName}
-        </Link>
-      ) : (
-        <Link
-          href="/profile/dashboard/employer/company"
-          className="inline-flex items-center rounded-md bg-slate-200 p-4 text-sky-700 hover:text-sky-500"
-        >
-          <p>Please set up your company</p>
-          <ChevronRight />
-        </Link>
-      )}
+    <div className="h-full bg-slate-50 p-6">
+      <div className="space-y-4 md:w-2/3">
+        <div className="justify-between gap-4 rounded-md bg-white p-8 shadow-[rgba(17,_17,_26,_0.1)_0px_0px_16px] md:flex">
+          <div className="flex-1">
+            <h1 className="my-4 flex items-center gap-4 text-2xl font-bold">
+              <BriefcaseIcon className="h-6 w-6 text-primary" />
+              {employer.employerProfile?.company?.companyName}
+              <Link
+                href={`/profile/dashboard/employer/company/${employer.employerProfile?.company?.id}`}
+              >
+                <Pencil className="h-6 w-6 text-primary" />
+              </Link>
+            </h1>
+            <p className="my-4 flex items-center gap-4 text-lg capitalize">
+              <Hammer className="h-6 w-6 text-primary" />
+              {employer.employerProfile?.company?.sector?.label}
+            </p>
+            <p className="my-4 flex items-center gap-4 text-lg capitalize">
+              <MapPin className="h-6 w-6 text-primary" />
+              {employer.address?.city}
+            </p>
+          </div>
+          <div className="flex-1">
+            <p className="my-4 flex items-center gap-4 text-sm">
+              <PhoneIcon className="h-6 w-6 text-primary" />
+              {employer.profile?.phoneNumber}
+            </p>
+            <p className="my-4 flex items-center gap-4 text-sm">
+              <Mail className="h-6 w-6 text-primary" />
+              {employer.email}
+            </p>
+          </div>
+        </div>
+        <div className="grid-cols-3 justify-between  gap-2 md:grid ">
+          <div className="flex w-full flex-col items-center rounded-md bg-white p-8 shadow-[rgba(17,_17,_26,_0.1)_0px_0px_16px]">
+            <Link
+              href={"/profile/dashboard/employer/jobs"}
+              className="my-4 flex items-center gap-4 border-b text-lg font-bold text-zinc-600 hover:text-secondary"
+            >
+              All Jobs
+              <ChevronRight className="h-6 w-6  hover:text-secondary" />
+            </Link>
+            <p className="text-[1.2rem] font-bold text-primary">
+              {employer.employerProfile?.company?.jobs?.length}
+            </p>
+          </div>
+          <div className="flex w-full flex-col items-center rounded-md bg-white p-8 shadow-[rgba(17,_17,_26,_0.1)_0px_0px_16px]">
+            <h2 className="my-4 flex items-center gap-4 border-b text-lg font-bold text-zinc-600">
+              Open Jobs
+            </h2>
+            <p className="text-[1.2rem] font-bold text-primary">
+              {
+                employer.employerProfile?.company?.jobs?.filter(
+                  (job) => job.status === JOBSTATUS.OPEN,
+                ).length
+              }
+            </p>
+          </div>
+          <div className="flex w-full flex-col items-center rounded-md bg-white p-8 shadow-[rgba(17,_17,_26,_0.1)_0px_0px_16px]">
+            <h2 className="my-4 flex items-center gap-4 border-b text-lg font-bold text-zinc-600">
+              Closed Jobs
+            </h2>
+            <p className="text-[1.2rem] font-bold text-primary">
+              {
+                employer.employerProfile?.company?.jobs?.filter(
+                  (job) => job.status === JOBSTATUS.OPEN,
+                ).length
+              }
+            </p>
+          </div>
+        </div>
+        <div className="flex w-[calc(33%-0.25rem)] flex-col items-center rounded-md bg-white p-8 shadow-[rgba(17,_17,_26,_0.1)_0px_0px_16px]">
+          <h2 className="my-4 flex items-center gap-4 border-b text-lg font-bold text-zinc-600">
+            Total Candidates
+          </h2>
+          <p className="text-[1.2rem] font-bold text-primary">
+            {employer.candidates?.length}
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
