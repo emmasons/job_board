f708d72e41aecbcd835322992f1bb31fae970254
xcixor <pndungu54@gmail.com>
2024-07-27 14:02:12 +0300

    Fix layout height issue in auth components
    Adjusted the height property in the AuthLayout component to ensure it spans the full height of its container, resolving an issue where content might not be properly centered or aligned. Additionally, standardized the whitespace in the unverified-email page component for consistency.

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="flex  w-full flex-1 flex-col rounded-md bg-[url('/auth/ladylogin.jpg')] bg-cover bg-center bg-no-repeat shadow sm:flex-row ">
+    <div className="flex h-full w-full flex-1 flex-col rounded-md bg-[url('/auth/ladylogin.jpg')] bg-cover bg-center bg-no-repeat shadow sm:flex-row ">
       {/* <div className="basis-1/2">
         <AuthHero />
       </div> */}
