9272d3dd7adc914b649cc7ebc5e03ffa96eca5c8
javaGhost1 <dennismurage97@gmail.com>
2024-08-01 15:37:06 +0300

    Update Job Seeker Dashboard and Footer Components
    Updated the job seeker dashboard to include a table view for applied jobs and job alerts, enhancing the user interface with more detailed information and interactive elements. Also, revised the footer components to improve clarity and provide more comprehensive information about the platform, including FAQs, about section, privacy policy, and terms and conditions. These changes aim to enhance user experience and provide better transparency regarding the platform's operations and policies.

--- app/profile/dashboard/job-seeker/applied-jobs/page.tsx
+++ app/profile/dashboard/job-seeker/applied-jobs/page.tsx
@@ -2,10 +2,54 @@ import { redirect } from "next/navigation";
 import { getCurrentSessionUser } from "@/lib/auth";
 
 import { Role } from "@prisma/client";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 
 import SavedJobs from "@/components/dashboard/job-seeker/cv/SavedJobs";
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 
 const page = async () => {
+  const invoices = [
+    {
+      title: "Territory Sales Officer",
+      status: "open",
+      date: "13/7/2024",
+      process: "Appplied",
+    },
+    {
+      title: "Tailor",
+      status: "Open",
+      date: "1/2/2024",
+      process: "shortlisted",
+    },
+    {
+      title: "UAE National - Internal Audit Assistant Manager",
+      status: "closed",
+      date: "7/5/2024",
+      process: " Hired ",
+    },
+    {
+      title: "Mechanical Helper",
+      status: "open",
+      date: "9/5/2024",
+      process: "Interview",
+    },
+    {
+      title: "Accountant",
+      status: "closed",
+      date: "12/12/2023",
+      process: "Hired",
+    },
+  ];
+ 
   const user = await getCurrentSessionUser();
   if (!user || !(user.role === Role.JOB_SEEKER)) {
     return redirect("/auth/signin?callbackUrl=/profile/dashboard/job-seeker");
@@ -13,7 +57,40 @@ const page = async () => {
 
 
   return (
-  <p>Applied jobs</p>
+    <MaxWidthWrapper>
+      <div className="py-16">
+        <h2 className="text-2xl ">Applied jobs</h2>
+      </div>
+
+      <Table>
+        <TableCaption>A list of your recently applied jobs</TableCaption>
+        <TableHeader className="bg-slate-100">
+          <TableRow>
+            <TableHead className="w-[300px]">Title</TableHead>
+            <TableHead className="text-right">Status</TableHead>
+            <TableHead className="text-right">Process</TableHead>
+            <TableHead className="text-right">Date Applied</TableHead>
+            <TableHead className="text-right">Action</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {invoices.map((invoice) => (
+            <TableRow key={invoice.title}>
+              <TableCell className="font-medium">{invoice.title}</TableCell>
+              <TableCell className="text-right">{invoice.status}</TableCell>
+              <TableCell className="text-right">{invoice.process}</TableCell>
+              <TableCell className="text-right">{invoice.date}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+        {/* <TableFooter>
+          <TableRow>
+            <TableCell colSpan={3}>Total</TableCell>
+            <TableCell className="text-right">$2,500.00</TableCell>
+          </TableRow>
+        </TableFooter> */}
+      </Table>
+    </MaxWidthWrapper>
   );
 };
 
--- app/profile/dashboard/job-seeker/job-alerts/page.tsx
+++ app/profile/dashboard/job-seeker/job-alerts/page.tsx
@@ -1,15 +1,88 @@
 import { redirect } from "next/navigation";
 import { getCurrentSessionUser } from "@/lib/auth";
-
+import { Label } from "@/components/ui/label";
+import { Switch } from "@/components/ui/switch";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 import { Role } from "@prisma/client";
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 
 const page = async () => {
   const user = await getCurrentSessionUser();
   if (!user || !(user.role === Role.JOB_SEEKER)) {
     return redirect("/auth/signin?callbackUrl=/profile/dashboard/job-seeker");
   }
+    const invoices = [
+      {
+        title: "Territory Sales Officer",
+        status: "open",
+        date: "13/7/2024",
+        process: "Appplied",
+      },
+      {
+        title: "Tailor",
+        status: "Open",
+        date: "1/2/2024",
+        process: "shortlisted",
+      },
+      {
+        title: "UAE National - Internal Audit Assistant Manager",
+        status: "closed",
+        date: "7/5/2024",
+        process: " Hired ",
+      },
+      {
+        title: "Mechanical Helper",
+        status: "open",
+        date: "9/5/2024",
+        process: "Interview",
+      },
+      {
+        title: "Accountant",
+        status: "closed",
+        date: "12/12/2023",
+        process: "Hired",
+      },
+    ];
 
-  return <p>Job Alerts</p>;
+  return (
+    <MaxWidthWrapper>
+      <div className="flex items-center space-x-2 py-16">
+        <Switch id="airplane-mode" />
+        <Label htmlFor="airplane-mode">Job Alerts</Label>
+      </div>
+      <Table>
+        <TableCaption>A list of your recently applied jobs</TableCaption>
+        <TableHeader className="bg-slate-100">
+          <TableRow>
+            <TableHead className="w-[300px]">Keyword</TableHead>
+            <TableHead className="text-right">Location</TableHead>
+            <TableHead className="text-right">Functional Area</TableHead>
+            <TableHead className="text-right">Create on</TableHead>
+            <TableHead className="text-right">Action</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {invoices.map((invoice) => (
+            <TableRow key={invoice.title}>
+              <TableCell className="font-medium">{invoice.title}</TableCell>
+              <TableCell className="text-right">{invoice.status}</TableCell>
+              <TableCell className="text-right">{invoice.process}</TableCell>
+              <TableCell className="text-right">{invoice.date}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </MaxWidthWrapper>
+  );
 };
 
 export default page;
