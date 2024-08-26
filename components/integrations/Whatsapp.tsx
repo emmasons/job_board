ea41ebcea3d7efd7a8b9e697f3902b0f7b5bced6
xcixor <pndungu54@gmail.com>
2024-08-26 11:20:48 +0300

    Add chat icon toggle functionality
    Introduce the ability to toggle the chat icon visibility on the WhatsApp button
    component. This allows users to show/hide the chat icon as needed for a better
    user experience.

--- components/integrations/Whatsapp.tsx
+++ components/integrations/Whatsapp.tsx
@@ -2,6 +2,8 @@
 
 import { Icon } from "@iconify/react";
 import React, { useState, useEffect } from "react";
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
 
 const WhatsAppButton = ({ phoneNumber, message }) => {
   const [isMobile, setIsMobile] = useState(false);
@@ -25,19 +27,39 @@ const WhatsAppButton = ({ phoneNumber, message }) => {
     setIsModalOpen(false);
   };
 
+  const [isChatIconOpen, setIsChatIconOpen] = useState(true);
+
+  const toggleChatIcon = () => {
+    setIsChatIconOpen(!isChatIconOpen);
+  };
+
   return (
     <>
-      <button
-        onClick={openModal}
-        className="fixed bottom-4 right-4 z-10 rounded-full bg-green-500 p-3 text-center font-bold text-white shadow-lg transition-colors duration-200 ease-in-out hover:bg-green-600"
-      >
-        <Icon icon="mdi:whatsapp" className="h-8 w-8" />
-      </button>
+      <div className="relative">
+        <button
+          onClick={openModal}
+          className={cn(
+            "fixed bottom-4 right-4 z-10 rounded-full bg-green-500 p-3 text-center font-bold text-white shadow-lg transition-colors duration-200 ease-in-out hover:bg-green-600",
+            !isChatIconOpen && "hidden",
+          )}
+        >
+          <Icon icon="mdi:whatsapp" className="h-8 w-8" />
+        </button>
+        <button onClick={closeModal} className="fixed bottom-1 right-4 z-10">
+          <Icon
+            icon={isChatIconOpen ? "mdi:close" : "mdi:plus-circle"}
+            className="h-4 w-4"
+            onClick={toggleChatIcon}
+          />
+        </button>
+      </div>
 
       {isModalOpen && (
         <div className="fixed bottom-20 right-4 z-50 translate-y-0 transform rounded-lg bg-white p-6 shadow-lg transition-transform duration-300">
           <div className="mb-4 flex items-center justify-between gap-12">
-            <h2 className="text-xl font-semibold">Infinite Talent Limited Support</h2>
+            <h2 className="text-xl font-semibold">
+              Infinite Talent Limited Support
+            </h2>
             <button
               onClick={closeModal}
               className="text-gray-700 hover:text-gray-900"
