e9200196539f0d9d0e84e2c08904a65322774a2a
xcixor <pndungu54@gmail.com>
2024-09-04 18:26:18 +0300

    Add CreateJobForm component and update form schema
    - Import CreateJobForm component in page.tsx
    - Update CreateJobForm component with new fields and logic for editing
    - Update form schema to handle number of positions as a number
    - Add conditional logic in CreateJobForm for editing existing job
    - Update onSubmit function in CreateJobForm to handle editing
    - Add loader animation and dynamic button text in CreateJobForm
    - Create migration to make startDate column nullable in Job table

--- app/profile/dashboard/employer/jobs/[id]/page.tsx
+++ app/profile/dashboard/employer/jobs/[id]/page.tsx
@@ -21,6 +21,7 @@ import { Role } from "@prisma/client";
 import { getEmployerCandidatesIds } from "@/actions/get-employer-candidates-ids";
 import { JobActions } from "@/components/dashboard/employer/jobs/JobActions";
 import { Banner } from "@/components/Banner";
+import CreateJobForm from "@/components/dashboard/employer/jobs/create/CreateJobForm";
 
 type Props = {
   params: {
@@ -128,7 +129,7 @@ const page = async ({ params, searchParams }: Props) => {
         </div>
       </div>
 
-      <EditJobForm
+      <CreateJobForm
         initialData={job}
         sectorList={sectors.map((sector) => ({
           label: sector.label,
@@ -144,6 +145,7 @@ const page = async ({ params, searchParams }: Props) => {
           label: exp.label,
           value: exp.id,
         }))}
+        isEditingJob={true}
       />
       <div className="rounded-md bg-slate-100 p-4">
         <h2 className="my-4 flex items-center gap-4 border-b text-lg font-bold text-zinc-600">
---
+++ prisma/migrations/20240904145909_062/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Job" ALTER COLUMN "startDate" DROP NOT NULL;
