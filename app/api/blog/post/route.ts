ab1808fc17a61b465b162d054a054ecf494bd92e
xcixor <pndungu54@gmail.com>
2024-09-13 16:01:53 +0300

    send notifications on create new post

--- app/api/blog/post/route.ts
+++ app/api/blog/post/route.ts
@@ -2,8 +2,10 @@
 import { NextResponse } from "next/server";
 import { db } from "@/lib/db";
 import { getCurrentSessionUser } from "@/lib/auth";
-import { Role } from "@prisma/client";
+import { NOTIFICATION_TYPES, Role, SUBSCRIPTION_TYPE } from "@prisma/client";
 import slugify from "slugify";
+import { sendEmail } from "@/lib/email";
+import { env } from "@/lib/env";
 
 export async function POST(req: Request) {
   try {
@@ -27,6 +29,42 @@ export async function POST(req: Request) {
         slug,
       },
     });
+
+    const subscriptions = await db.subscription.findMany({
+      where: {
+        type: SUBSCRIPTION_TYPE.NEWSLETTER,
+      },
+    });
+
+    const users = await db.user.findMany({
+      where: {
+        email: {
+          in: subscriptions.map((sub) => sub.email),
+        },
+      },
+    });
+    const notifications = users.map((user) => ({
+      userId: user.id,
+      type: NOTIFICATION_TYPES.NEW_BLOG_POST,
+      title: post.title,
+      summary: post.epigraph,
+      resourceId: `${env.BASE_DOMAIN}/blog/${post.slug}`,
+      fromId: author.id,
+    }));
+    await db.notification.createMany({ data: notifications });
+    await Promise.all(
+      subscriptions.map(async (subscription) =>
+        sendEmail({
+          to_email: subscription.email,
+          subject: post.title,
+          message: `
+          <h1>${post.title}</h1>
+          <p>${post.epigraph}</p>
+          <a href="${env.BASE_DOMAIN}/blog/${post.slug}">Read more</a>
+          `,
+        }),
+      ),
+    );
 
     return NextResponse.json({ id: post.id, message: "Post created" });
   } catch (error) {
