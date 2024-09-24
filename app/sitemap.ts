f4fc17278726f096436f0da3853119fabed002ba
xcixor <pndungu54@gmail.com>
2024-09-24 12:28:12 +0300

    Add robots.txt and sitemap.xml generation logic
    Create functions to generate robots.txt and sitemap.xml files with dynamic
    content based on environment variables and post data. This will improve SEO
    and site indexing.

---
+++ app/sitemap.ts
@@ -0,0 +1,58 @@
+import { MetadataRoute } from "next";
+import { env } from "@/lib/env";
+import { getAllPosts } from "@/actions/get-all-posts";
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const posts = await getAllPosts();
+  const postEntries = posts.map((post) => ({
+    url: `${env.BASE_DOMAIN}blog/${post.slug}`,
+    lastModified: post.updatedAt.toISOString(),
+    priority: 0.7,
+  }));
+  return [
+    {
+      url: `${env.BASE_DOMAIN}`,
+      lastModified: new Date().toISOString(),
+      priority: 1.0,
+    },
+    {
+      url: `${env.BASE_DOMAIN}FAQs`,
+      lastModified: new Date().toISOString(),
+      priority: 1.0,
+    },
+    {
+      url: `${env.BASE_DOMAIN}contact`,
+      lastModified: new Date().toISOString(),
+      priority: 1.0,
+    },
+    {
+      url: `${env.BASE_DOMAIN}find-candidates`,
+      lastModified: new Date().toISOString(),
+      priority: 1.0,
+    },
+    {
+      url: `${env.BASE_DOMAIN}privacy-policy`,
+      lastModified: new Date().toISOString(),
+      priority: 1.0,
+    },
+    {
+      url: `${env.BASE_DOMAIN}search`,
+      lastModified: new Date().toISOString(),
+      priority: 1.0,
+    },
+    {
+      url: `${env.BASE_DOMAIN}terms-and-conditions`,
+      lastModified: new Date().toISOString(),
+      priority: 0.8,
+    },
+    {
+      url: `${env.BASE_DOMAIN}blog`,
+    },
+    {
+      url: `${env.BASE_DOMAIN}about`,
+      lastModified: new Date().toISOString(),
+      priority: 0.8,
+    },
+    ...postEntries,
+  ];
+}
