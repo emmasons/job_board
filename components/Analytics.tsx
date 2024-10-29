be0bda93cf04e2aded53a3fa3b22640bc2704a87
xcixor <pndungu54@gmail.com>
2024-10-29 21:32:39 +0300

    Add Analytics component and update sector labels
    Added Analytics component to include Google Analytics tracking code in the application. Updated sector labels in the seed script for better clarity and consistency.

---
+++ components/Analytics.tsx
@@ -0,0 +1,20 @@
+import Script from "next/script";
+
+export default function Analytics() {
+  return (
+    <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+        `}
+      </Script>
+    </>
+  );
+}
