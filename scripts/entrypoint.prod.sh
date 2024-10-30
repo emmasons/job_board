c4496a4e9f63d2f2c84cc3ea4e7faaf122669fd6
xcixor <pndungu54@gmail.com>
2024-10-30 14:05:08 +0300

    Set permissions for Node.js user in entrypoint.prod.sh
    Add commands to set permissions for Node.js user in entrypoint.prod.sh script
    to ensure proper access to files.

--- scripts/entrypoint.prod.sh
+++ scripts/entrypoint.prod.sh
@@ -4,6 +4,10 @@ echo 'Starting app...'
 npx prisma generate
 npx prisma migrate deploy
 
+# Set permissions for the Node.js user
+RUN chmod 777 ./node_modules/.prisma/client/index.js
+RUN chown node:node ./node_modules/.prisma/client/schema.prismas
+
 # Fork a new process to run the backup script
 (
   node db_backup.ts
