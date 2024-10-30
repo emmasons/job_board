b134afbd9e2c2d1a06e1fb347b1ae7d81fbe8538
xcixor <pndungu54@gmail.com>
2024-10-30 14:15:35 +0300

    Update file permissions in entrypoint.prod.sh
    Change file permission commands to remove unnecessary "RUN" statements and
    correct the file path for setting permissions in entrypoint.prod.sh.

--- scripts/entrypoint.prod.sh
+++ scripts/entrypoint.prod.sh
@@ -5,8 +5,8 @@ npx prisma generate
 npx prisma migrate deploy
 
 # Set permissions for the Node.js user
-RUN chmod 777 ./node_modules/.prisma/client/index.js
-RUN chown node:node ./node_modules/.prisma/client/schema.prismas
+chmod 777 ./node_modules/.prisma/client/index.js
+chown node:node ./node_modules/.prisma/client/schema.prismas
 
 # Fork a new process to run the backup script
 (
