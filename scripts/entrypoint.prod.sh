15d09d0da9b8dc62f28bdd0265c80847d4682426
xcixor <pndungu54@gmail.com>
2024-10-30 15:12:05 +0300

    Update Dockerfile and entrypoint script permissions
    Modify Dockerfile to set permissions for node_modules directory
    and update entrypoint script to comment out unnecessary permission
    changes.

--- scripts/entrypoint.prod.sh
+++ scripts/entrypoint.prod.sh
@@ -5,8 +5,8 @@ npx prisma generate
 npx prisma migrate deploy
 
 # Set permissions for the Node.js user
-chmod -R 777 ./node_modules
-chown node:node ./node_modules/.prisma/client/schema.prisma
+# chmod -R 777 ./node_modules
+# chown node:node ./node_modules/.prisma/client/schema.prisma
 
 # Fork a new process to run the backup script
 (
