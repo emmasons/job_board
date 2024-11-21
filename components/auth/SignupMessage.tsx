74ebcc5ce8d53a29a728fcde0d22875a861182d4
xcixor <pndungu54@gmail.com>
2024-11-21 12:01:06 +0300

    Refactor signup page layout and improve responsiveness
    - Introduced `MaxWidthWrapper` component to wrap the signup page content, ensuring consistent layout across different screen sizes.
    - Adjusted the layout to use a flexbox structure, dividing the page into two halves: one for the signup message and one for the registration options.
    - Enhanced responsiveness by adding `max-md` class modifiers to adjust styles for smaller screens, such as centering text and reducing font size for list items.
    - Updated the signup message component to better align with the new layout, including centering text and adjusting the logo container size.
    - Changed the heading text from "Job Board" to "Create an Account" to better reflect the purpose of the page.

--- components/auth/SignupMessage.tsx
+++ components/auth/SignupMessage.tsx
@@ -2,18 +2,18 @@ import { Logo } from "@/components/navbar/Logo";
 
 const SignupMessage = () => {
   return (
-    <div className="flex w-auto flex-col justify-center text-white">
+    <div className="flex w-auto flex-col justify-center text-white max-md:items-center gap-4">
       <span className="inline-flex h-[100px] w-[100px] items-center justify-center rounded-full bg-white ">
         <Logo />
       </span>
       <div className="mb-8">
-        <h1 className="text-[2rem] font-bold max-md:text-[1.5rem]">
+        <h1 className="text-[2rem] font-bold max-md:text-center">
           Join 40,000+ companies hiring on jobsconnect.com
         </h1>
-        <p className="text-sm">No credit card required.</p>
+        <p className="text-xl max-md:text-center">No credit card required.</p>
       </div>
-      <h2 className="text-xl max-md:text-[1rem] font-semibold">Our hiring platform helps you to:</h2>
-      <ul className="mt-2 list-inside list-disc space-y-2 max-md:text-sm max-md:space-y-1">
+      <h2 className="text-2xl">Our hiring platform helps you to:</h2>
+      <ul className="mt-2 list-inside list-disc space-y-2 max-md:text-[0.8rem]">
         <li>Post jobs easily across locations.</li>
         <li>Reach 10 million+ monthly visitors.</li>
         <li>Hire faster with AI-powered screening.</li>
