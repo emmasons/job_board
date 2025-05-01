241bd5f0cb83f92985d050d11d668a3eb318c00d
xcixor <pndungu54@gmail.com>
2025-05-01 21:35:02 +0300

    fix: update PostImageForm to use placeholder image and adjust image loading logic

--- components/dashboard/admin/post/PostImageForm.tsx
+++ components/dashboard/admin/post/PostImageForm.tsx
@@ -55,16 +55,11 @@ export default function PostImageForm({
           </div>
         ) : (
           <div className="relative mt-2 aspect-video">
-            {/* <Image
+            <Image
               alt="Upload"
               fill
               className="rounded-md object-cover"
-              src={gcpData.downloadUrl}
-            /> */}
-            <img
-              alt="Upload"
-              className="rounded-md object-cover"
-              src={gcpData.downloadUrl}
+              src={gcpData.downloadUrl || "/assets/placeholder-image.png"}
             />
           </div>
         ))}
